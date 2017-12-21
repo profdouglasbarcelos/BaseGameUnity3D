0dadaf706d8dc92b336707fb0ac109bcb03c6e8b
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-12-21 15:10:27 -0200

    Criando classes GameManager e BaseGameState #resolve #3 resolve #4

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// GameManager eh a maquina de estados, trocando os estados de acordo com o estado atual do jogo.
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    // Implementacao Singleton
+    protected static GameManager gmInstance;
+    public static GameManager GMInstance
+    {
+        get { return gmInstance;  }
+    }
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/GameManager/GameStates/BaseGameState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class BaseGameState : MonoBehaviour
+{
+    [HideInInspector]
+    public GameManager gm;
+
+    [HideInInspector]
+    public string stateName;
+
+    public abstract void OnEnter(BaseGameState from);
+    public abstract void OnExit(BaseGameState to);
+    public abstract void OnTick();
+
+    public string GetName()
+    {
+        return stateName;
+    }
+}
